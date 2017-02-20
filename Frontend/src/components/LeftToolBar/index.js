fb3454d7b639a6da7da19efdf41749c9b3350ac4
zhaochenjun <holyoops@hotmail.com>
2017-02-21 00:12:26 +0800

    commit

--- Frontend/src/components/LeftToolBar/index.js
+++ Frontend/src/components/LeftToolBar/index.js
@@ -1,66 +1,32 @@
 import React, { Component } from 'react';
-// import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import Menu from 'components/Menu';
 import './index.less';
 
-class SubTitle extends Component {
-
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        transiton: 'sub-title move'
-      };
-    }
-
-    // componentDidAppear(){
-    //   console.log(1111);
-    //   this.setState({
-    //     transiton: 'sub-title move'
-    //   });
-    // }
-
-    componentWillAppear(callback) {
-        console.log('will appear');
-        callback();
-    }
-    componentDidAppear() {
-        console.log('did appear');
-    }
-
-    componentWillEnter(callback) {
-        callback();
-        console.log('will enter');
-    }
-    componentDidEnter() {
-        console.log('did enter');
-    }
-    componentWillLeave(callback) {
-        callback();
-        console.log('wiil leave');
-    }
-    componentDidLeave() {
-        console.log('did leave');
-    }
-    componentWillUnmount() {
-        console.log('will unmount');
-    }
-
-    render(){
-        return(
-          <div className={this.state.transiton}>
-            <div className='for'>
-              for
-            </div>
-            <div className='open-pletform'>
-              开放平台
-            </div>
-          </div>
-        );
-    }
-}
-
 class LeftToolBar extends Component {
+
+  componentWillAppear(callback) {
+      console.log('will appear');
+  }
+  componentDidAppear() {
+      console.log('did appear');
+  }
+
+  componentWillEnter(callback) {
+      console.log('will enter');
+  }
+  componentDidEnter() {
+      console.log('did enter');
+  }
+  componentWillLeave(callback) {
+      console.log('wiil leave');
+  }
+  componentDidLeave() {
+      console.log('did leave');
+  }
+  componentWillUnmount() {
+      console.log('will unmount');
+  }
 
   render() {
     return (
@@ -69,10 +35,25 @@ class LeftToolBar extends Component {
           <div className='HRAnalysis'>
             HRAnalysis
           </div>
-          <SubTitle />
+          <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}>
+            <div className='sub-title' key='sub-title'>
+              <div className='for'>
+                for
+              </div>
+              <div className='open-pletform'>
+                开放平台
+              </div>
+            </div>
+          </ReactCSSTransitionGroup>
+
         </div>
         <div className="menu-container">
-        <Menu/>
+          <Menu/>
         </div>
       </div>
     );
